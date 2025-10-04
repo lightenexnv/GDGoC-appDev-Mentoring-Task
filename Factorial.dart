38554c5146182822f930c2d8cbbab2b5e8b079b6
lightenexnv <svak3456@gmail.com>
2025-10-04 20:48:38 +0530

    Factorial Task Done

---
+++ Factorial.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  print("Enter a number:");
+  int n = int.parse(stdin.readLineSync()!);
+  int fact = 1;
+  for (int i = 1; i <= n; i++) fact *= i;
+  print("Factorial: $fact");
+}
